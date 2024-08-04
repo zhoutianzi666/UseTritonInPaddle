96fa268c6e4247268640928530067b4c7e5a381e
zhoutianzi666 <17801055074@163.com>
2024-08-04 04:16:24 +0000

    add try except

--- use_triton_in_paddle/api.py
+++ use_triton_in_paddle/api.py
@@ -39,10 +39,12 @@ def make_triton_compatible_with_paddle():
                     copy1_line = line
                     copy2_line = line
                     copy3_line = line
-                    copy0_line.replace("import torch", "try:")
-                    copy1_line.replace("import torch", "\timport torch")
-                    copy2_line.replace("import torch", "execpt:")
-                    copy3_line.replace("import torch", "\timport use_triton_in_paddle as torch")
+                    
+                    
+                    copy0_line = copy0_line.replace("import torch", "try:")
+                    copy1_line = copy1_line.replace("import torch", "\timport torch")
+                    copy2_line = copy2_line.replace("import torch", "execpt:")
+                    copy3_line = copy3_line.replace("import torch", "\timport use_triton_in_paddle as torch")
                     new_all_lines.append(copy0_line)
                     new_all_lines.append(copy1_line)
                     new_all_lines.append(copy2_line)
